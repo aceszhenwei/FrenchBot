810d33f280fc5aaff72418797b45f29bbcdd2084
Alexander Er <aceszhenwei@yahoo.com>
2022-01-28 01:50:12 +0800

    Updated Accent Access
    Allowed for non-accented answers to be accepted.

--- src/com/company/FrenchBot.java
+++ src/com/company/FrenchBot.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.text.Normalizer;
 import java.util.*;
 
 public class FrenchBot {
@@ -41,16 +42,24 @@ public class FrenchBot {
         while (upperBound > 0) {
             int randInt = rand.nextInt(upperBound);
             System.out.println("What is the passe compose of: " + arr.get(randInt)[0]);
-            String input = scanner.nextLine();
-            String answer = arr.get(randInt)[1].toLowerCase(Locale.ROOT);
-            String receivedAnswer = input.toLowerCase(Locale.ROOT);
+            // Get input
+            String accentedInput = scanner.nextLine();
+            // Normalize Input
+            String input = accentedInput.toLowerCase(Locale.ROOT);
+            input = Normalizer.normalize(input, Normalizer.Form.NFD);
+            String receivedAnswer = input.replaceAll("[^\\p{ASCII}]", "");
+            // Get Answer
+            String accentedAnswer = arr.get(randInt)[1].toLowerCase(Locale.ROOT);
+            // Normalize Answer
+            String answer = Normalizer.normalize(accentedAnswer, Normalizer.Form.NFD);
+            answer = answer.replaceAll("[^\\p{ASCII}]", "");
             upperBound -= 1;
             if (receivedAnswer.equals("exit")) {
                 System.out.println("Aw. Leaving so soon?");
                 break;
             }
             else if (receivedAnswer.equals(answer)) {
-                System.out.println("Correct!");
+                System.out.println("Correct! It is indeed: " + arr.get(randInt)[1]);
                 score += 1;
                 arr.remove(randInt);
             }
